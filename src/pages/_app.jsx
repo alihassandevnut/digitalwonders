12ab1a471c38402c9c1f6915d0ef657777b0e783
alihassandevnut <alihassan.devnut@gmail.com>
2023-11-15 10:02:21 +0400

    added meta info

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,16 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/globals.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
-        rel="stylesheet"
-      />
+      <Head>
+        <link rel="shortcut icon" href="/static/favicon.svg" />
+        <title>Digital Wonders</title>
+      </Head>
+      <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet" />
       <Component {...pageProps} />
     </>
   );
